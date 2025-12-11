457d7c043965218fd6b95680fca0cebccc31445e
gvasquezfigueroa05 <gvasquezfigueroa@gmail.com>
2025-12-11 12:47:24 -0500

    modified providers.tf and main.tf to set up multiple providers and create EC2 instances in different regions.

--- providers.tf
+++ providers.tf
@@ -1,21 +1,23 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = ">= 5.31.0"
+      source  = "hashicorp/aws"
+      version = ">=5.31.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
-  profile = "Helen"
+  region  = "us-west-2"
+  profile = "Gvasquez"
   # Configuration options
 }
 
+## Add another provider:
+
 provider "aws" {
-  alias = "west"
-  region = "us-west-1"
-  profile = "Helen"
+  alias   = "west-1"
+  region  = "us-west-1"
+  profile = "Gvasquez"
 }
 
