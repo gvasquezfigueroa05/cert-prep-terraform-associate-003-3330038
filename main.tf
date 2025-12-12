465bd28aef7c77567fa885b774c05e4346d26c32
gvasquezfigueroa05 <gvasquezfigueroa@gmail.com>
2025-12-12 13:19:59 -0500

    import ec2 instance to terraform state

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
-resource "aws_instance" "MyInstance_test" {
+resource "aws_instance" "ldap-ubuntu" {
     #instance configuration
+    ami = "ami-00f46ccd1cbfb363e"
+    instance_type = "t3.micro"
+
+    tags = {
+        Name = "ldap-ubuntu"
+    }
+
 }
